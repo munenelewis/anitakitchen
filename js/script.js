8acbb3ebd64a0d237f799b2967943f33950a06b6
Lewis <munene2379@gmail.com>
2017-11-04 09:54:39 +0300

    made the pages more responsive

--- js/script.js
+++ js/script.js
@@ -1,17 +1,7 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
